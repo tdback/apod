1c3bfb45fa886d453b0423a35be7bd43ac2ca061
tdback <tyler@tdback.net>
2024-11-04 21:09:34 -0500

    can now build script with flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        bin = "apod";
+        version = "1.1.0";
         pkgs = import nixpkgs { inherit system; };
       in
       {
@@ -20,7 +22,39 @@
                 requests
               ]
             ))
+            ruff
           ];
+        };
+
+        defaultPackage = pkgs.python3Packages.buildPythonApplication {
+          pname = bin;
+          version = version;
+          src = ./.;
+
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            beautifulsoup4
+            requests
+          ];
+
+          preBuild = ''
+            cat > setup.py << EOF
+            from setuptools import setup
+
+            setup(
+                name='${bin}',
+                version='${version}',
+                install_requires=[
+                    'beautifulsoup4',
+                    'requests',
+                ],
+                scripts=['${bin}.py'],
+            )
+            EOF
+          '';
+
+          postInstall = ''
+            mv -v $out/bin/${bin}.py $out/bin/${bin}
+          '';
         };
       });
 }
